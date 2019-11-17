5af0798131f83d73d1fb453ea5f8ac2ad84c00c7
Travis Avery <travisavery82@gmail.com>
2019-11-17 10:54:37 -0600

    making pip installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='tableformatters',
+    version='1.0.0',
+    author='Travis Avery',
+    python_requires='>=3.7',
+    packages=find_packages()
+)
